67ceac9ef16e12256a8d71a44b38f61b2ce4919f
David Obaro <David.obaro@bark.com>
2023-08-09 17:00:36 +0100

    clicking accept all cookies

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,6 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://qa-7.d.bark.com/en/gb/')
-    cy.get('#onetrust-accept-btn-handler')
+    cy.get('#onetrust-accept-btn-handler').click()
   })
 })
